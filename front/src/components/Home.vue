9a5dd6ce83635132a00376ca59622dd4880e177d
Rodion <dolrodion@gmail.com>
2023-11-13 22:58:38 +0300

    rd-01:настроил дефолный роутинг

---
+++ front/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+     <p>ddddd</p>
+     <p>ddddd</p>
+
+     <p>ddddd</p><p>ddddd</p>
+     <p>ddddd</p>
+     <p>ddddd</p>
+
+     <p>ddddd</p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  
+  </script>
